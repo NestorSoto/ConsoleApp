197b5746ef5fdff87cf0cf9f8e0eedce5919c089
Néstor <66892045+NestorSoto@users.noreply.github.com>
2021-11-10 17:43:05 -0500

    funcion mostrar aviones agregada

--- ConsoleApp/Vistas/Administrador.cs
+++ ConsoleApp/Vistas/Administrador.cs
@@ -18,7 +18,9 @@ namespace ConsoleApp.Vistas
         public static void MenuAdmin()
         {
             Console.WriteLine("Mostrando menu de opciones de administrador");
-            Task t= showCitiesAsync();
+            showCitiesAsync();
+            showAirplane();
+
 
         }
 
@@ -36,6 +38,21 @@ namespace ConsoleApp.Vistas
                 Console.WriteLine(reader.GetValue(1) + " || " + reader.GetValue(2));
             }
         }
-        
+        static async Task showAirplane()
+        {
+            using var connection = mysql.getConexion();
+            await connection.OpenAsync();
+
+            using var command = new MySqlCommand("SELECT * FROM avion;", connection);
+            using var reader = await command.ExecuteReaderAsync();
+            Console.WriteLine("Mostrando lista de Aviones \n Marca || modelo || Capacidad");
+            while (await reader.ReadAsync())
+            {
+
+                Console.WriteLine(reader.GetValue(2) + " || " + reader.GetValue(3)+" || "+ reader.GetValue(1));
+            }
+        }
+
+
     }
 }
